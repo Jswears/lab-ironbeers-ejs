96eb823373cdf38c009bbad152195b69edd82487
Jswears <ji.swearssalinas@gmail.com>
2023-07-04 17:57:43 +0200

    done

--- app.js
+++ app.js
@@ -18,5 +18,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beersFromApi });
+    })
+    .catch(error => console.log('the error is', error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beer => {
+      res.render('random-beer', { beer: beer[0] });
+    })
+    .catch(error => console.log('the error is', error));
+});
+
+app.get('beers/:id', (req, res) => {
+  const beerId = req.params.id;
+  punkAPI
+    .getBeer(beerId)
+    .then(beer => {
+      res.render('random_beer', { beer: beer[0] });
+    })
+    .catch(error => console.log('the error is', error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
